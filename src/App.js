996065c458ad7adcc2153f4cc1dfa473a4f5c83c
brisacazares17 <79296074+brisacazares17@users.noreply.github.com>
2022-03-29 17:17:42 -0700

    finalizado
    brisa y manuel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <h1>Hola Mundo, Brisa Cazares</h1>
+    <h1>Hola Mundo, Brisa Cazares y Manuel Torres</h1>
     
   );
 }
